5131e7221682a98e5d8988efed1957a61ea535c7
Zhijie Xu <jennyxu000@gmail.com>
2020-05-11 04:10:15 -0700

    go back to top animation

--- js/main.js
+++ js/main.js
@@ -2,6 +2,16 @@
 //   e.target.style.color = "red";
 // });
 
+//jQuery codes
 $("section").on("click", function () {
   $(this).toggleClass("show-description");
+});
+
+//scroll function
+$(document).ready(function () {
+  $("#gotop").click(function (event) {
+    event.preventDefault();
+    $("html, body").animate({ scrollTop: 0 }, "slow");
+    return false;
+  });
 });
