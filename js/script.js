f702bca4da5a30c04ca9ff0b5fdaca73342b074d
Zhijie Xu <jennyxu000@gmail.com>
2020-06-11 02:58:50 -0700

    hamburger menu works

--- js/script.js
+++ js/script.js
@@ -1,15 +1,32 @@
 // Test if jQuery and script works
 // console.log("hello", $);
 $(() => {
-  // $(".official_contents").hide();
-  $(".entrance_page").hide();
+  const $official_contents = $(".official_contents_wrapper");
+  const $entrance_page = $(".entrance_page");
+  const $welcome_button = $("#welcomeButton");
+  const $hamburger = $(".hamburger");
+  const $hamburgerbar1 = $(".bar1");
+  const $hamburgerbar2 = $(".bar2");
+  const $hamburgerbar3 = $(".bar3");
+  const $dropdown = $(".dropdown");
 
-  $("#welcomeButton").on("click", () => {
+  // $official_contents.hide();
+  $entrance_page.hide();
+
+  $welcome_button.on("click", () => {
     // Slide up animation: entrance page gone, official contents appear
-    $(".entrance_page").fadeOut("300", "linear", () => {
-      $(".official_contents").show();
-      console.log("clicked?");
+    $entrance_page.fadeOut("300", "linear", () => {
+      $official_contents.show();
+      // console.log("clicked?");
     });
+  });
+
+  // hamburger animation
+  $hamburger.on("click", () => {
+    $hamburgerbar1.toggleClass("changebar1");
+    $hamburgerbar2.toggleClass("changebar2");
+    $hamburgerbar3.toggleClass("changebar3");
+    $dropdown.toggleClass("dropdown_show");
   });
 
   //make a div clickable
